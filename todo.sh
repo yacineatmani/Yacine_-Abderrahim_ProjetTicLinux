8a83e08e0407a9eb812423f05aac31f5d424e6ab
yacineatmani <yacineatmani1080@gmail.com>
2024-10-04 16:12:08 +0200

    ajout commentaire

--- todo.sh
+++ todo.sh
@@ -1,28 +1,42 @@
 #!/bin/bash
 
+# Affiche un message de bienvenue
 echo "Bienvenue dans votre gestionnaire de tâches!"
 
+# Boucle infinie pour maintenir le programme en cours d'exécution
 while true; do
     echo "
     1. Ajouter une tâche
     2. Afficher les tâches
     3. Quitter
     "
-echo "Choisissez une option (1/2/3): "
-read choix
-if [ "$choix" = "1" ]; then
+
+    # Demande à l'utilisateur de choisir une option
+    echo "Choisissez une option (1/2/3): "
+    read choix
+
+    # Si l'utilisateur choisit d'ajouter une tâche
+    if [ "$choix" = "1" ]; then
         echo "Entrez votre tâche:"
         read tache
+        # Ajoute la tâche au fichier tasks.txt
         echo "$tache" >> tasks.txt
         echo "Tâche ajoutée!"
- elif [ "$choix" = "2" ]; then
+
+    # Si l'utilisateur choisit d'afficher les tâches
+    elif [ "$choix" = "2" ]; then
         echo "Voici vos tâches:"
+        # Affiche le contenu du fichier tasks.txt
         cat tasks.txt
 
- elif [ "$choix" = "3" ]; then
+    # Si l'utilisateur choisit de quitter
+    elif [ "$choix" = "3" ]; then
         echo "Au revoir!"
-        exit
-else
+        exit  # Quitte le script
+
+    # Si l'utilisateur choisit une option invalide
+    else
         echo "Option non valide. Veuillez réessayer."
     fi
 done
+
